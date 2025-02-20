beaa65fc3299b72c3c64e97232de2779ce43b344
knyaz20 <nikitaknazev10211@gmail.com>
2025-02-20 14:33:54 +0300

    Add files via upload

---
+++ Task_S02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int a, b, c;
+    std::cout << "Write number 1: ";
+    std::cin >> a;
+    std::cout << "Write number 2: ";
+    std::cin >> b;
+    c = 10 / 3;
+    std::cout << "Celochislennoe delenie: ";
+    std::cout << c << std::endl;
+    c = 10 % 3;
+    std::cout << "Ostatok ot delenia: ";
+    std::cout << c << std::endl;
+}
